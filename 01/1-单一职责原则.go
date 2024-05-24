e22091dc7d59205b9f3064874d616650e9072348
huzejun <961639130@qq.com>
2024-05-25 03:35:20 +0800

    设计模式-单一职责原则-2024-5-25 03:35:11

--- 01/1-单一职责原则.go
+++ 01/1-单一职责原则.go
@@ -1,6 +1,57 @@
 // @Author huzejun 2024/5/24 20:23:00
 package main
 
-func main() {
+import "fmt"
 
+type ClothesShop struct {
 }
+
+type ClothesWork struct {
+}
+
+func (cs *ClothesShop) Style() {
+	fmt.Println("逛街的装扮")
+}
+
+func (cs *ClothesWork) Style() {
+	fmt.Println("逛街的装扮")
+}
+
+func main() {
+	//工作的业务
+	cw := ClothesWork{}
+	cw.Style()
+
+	//逛街的业务
+	cs := ClothesShop{}
+	cs.Style()
+}
+
+/*
+// 穿衣服的方式
+type Clothes struct {
+	//逛街穿衣服
+}
+
+// 工作穿衣服
+func (c *Clothes) OnWork() {
+	fmt.Println("工作的装扮")
+}
+
+// 逛街穿衣服
+func (c *Clothes) OnShop() {
+	fmt.Println("逛街的装扮")
+}
+
+func main() {
+	c := Clothes{}
+
+	//工作的业务
+	fmt.Println("在工作...")
+	c.OnWork()
+
+	//逛街的业务
+	fmt.Println("在逛街...")
+	c.OnShop()
+}
+*/
